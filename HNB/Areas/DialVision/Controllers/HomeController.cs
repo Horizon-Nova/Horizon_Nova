14660bed20d9bdcc9b99c633c6d7639c89034680
RuanLuojie <zooz74114@gmail.com>
2025-07-24 01:29:11 +0800

    2025-07-24_01-29-11

---
+++ HNB/Areas/DialVision/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HNB.Areas.DialVision.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("DialVision")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
