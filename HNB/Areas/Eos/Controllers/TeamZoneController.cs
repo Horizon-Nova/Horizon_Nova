9dc8b1ff7a2c94328c18c5c2e958124ee67f5aa4
RuanLuojie <hiaconde@gmail.com>
2025-07-25 09:53:46 +0800

    2025-07-25_09-53-45

---
+++ HNB/Areas/Eos/Controllers/TeamZoneController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HNB.Areas.Eos.Controllers
+{
+    [Area("Eos")]
+    public class TeamZoneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
