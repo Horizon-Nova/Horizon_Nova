5daf819dc1b89df7ce4a0488b872c5a07095a080
RuanLuojie <hiaconde@gmail.com>
2025-11-13 15:58:42 +0800

    2025-11-13_15-58-42

---
+++ HNB/Areas/NovaAPP/Controllers/DialVisionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HNB.Areas.NovaAPP.Controllers;
+
+[Area("NovaAPP")]
+public class DialVisionController : Controller
+{
+    public IActionResult Index() => View();
+
+    public IActionResult LoadScreen(string screen)
+    {
+        var screenPath = screen?.ToLower() switch
+        {
+            "login" => "Partials/DialVision/Screens/_LoginScreen",
+            "dashboard" => "Partials/DialVision/Screens/_DashboardScreen",
+            "register" => "Partials/DialVision/Screens/_RegisterScreen",
+            "camera" => "Partials/DialVision/Screens/_CameraScreen",
+            "settings" => "Partials/DialVision/Screens/_SettingsScreen",
+            _ => "Partials/DialVision/Screens/_LoginScreen"
+        };
+
+        return PartialView(screenPath);
+    }
+
+    public IActionResult LoadCalendar(int? year = null, int? month = null, string? selectedDate = null)
+    {
+        ViewBag.Year = year ?? DateTime.Now.Year;
+        ViewBag.Month = month ?? DateTime.Now.Month;
+        ViewBag.SelectedDate = selectedDate;
+        return PartialView("Partials/DialVision/_Calendar");
+    }
+
+    public IActionResult LoadTimeWheel(string? selectedTime = null)
+    {
+        ViewBag.SelectedTime = selectedTime;
+        return PartialView("Partials/DialVision/_TimeWheel");
+    }
+}
---
+++ HNB/wwwroot/NovaAPP/DialVision/js/settings-screen.js
@@ -0,0 +1,9 @@
+// SettingsScreen 專用 JavaScript
+$(function () {
+	if (window.lucide?.createIcons) {
+		window.lucide.createIcons();
+	}
+	// 若之後接 API，可在此載入使用者資訊與統計
+});
+
+
