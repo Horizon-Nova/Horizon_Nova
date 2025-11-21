ba825f0f60b184d514e53cab39c8d1c96075e095
RuanLuojie <hiaconde@gmail.com>
2025-11-21 18:07:13 +0800

    2025-11-21_18-07-13

--- HNB/Areas/Weather/Controllers/EntryController.cs
+++ HNB/Areas/Weather/Controllers/EntryController.cs
@@ -6,12 +6,12 @@ namespace HNB.Areas.Weather.Controllers;
 /// Weather 區域的服裝搭配控制器，負責處理服裝搭配相關的頁面顯示功能
 /// </summary>
 [Area("Weather")]
-public class OutfitController : Controller
+public class EntryController : Controller
 {
     /// <summary>
-    /// 顯示服裝搭配首頁
+    /// 顯示入口功能頁面（Try it for Free 進入）
     /// </summary>
-    /// <returns>返回首頁視圖</returns>
+    /// <returns>返回入口功能視圖</returns>
     public IActionResult Index()
         => View();
 }
